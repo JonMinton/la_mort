fc58cc9be2f2873c69b3760cfd0603f2ddbdf0a0
Jonathan Minton <nate.minton@gmail.com>
2015-05-21 19:08:13 +0100

    start to do lifetable

--- script.R
+++ script.R
@@ -19,19 +19,54 @@ require(ggplot2)
 require(lattice)
 
 
-# Data --------------------------------------------------------------------
+# Create tidy data (Run once only) --------------------------------------------------------------------
+# 
+# dta <- read.csv("data/unzipped/MYEB2_detailed_components_of_change_series_EW_(0213).csv") %>%
+#   tbl_df
+# 
+# 
+# data_tidied <- dta  %>% 
+#   gather(key=label, value=count, -lad2013_code, -lad2013_name, -country, -sex, -age)  %>% 
+#   mutate(
+#     year = str_extract(label, "\\d{1,4}"), 
+#     category = str_replace(str_extract(label, "\\D*"), "_$", ""),
+#     sex=ifelse(sex==1, "male", "female"))   %>% 
+#   select(lad2013_code, country, sex, age, year, category, count) %>%
+#   spread(category, count)
+# 
+# write.csv(data_tidied, file="data/tidied/england_la_count.csv", row.names=FALSE)
+# 
 
-dta <- read.csv("data/unzipped/MYEB2_detailed_components_of_change_series_EW_(0213).csv") %>%
+# e50, e65 and e80 by la, year and age ------------------------------------
+
+
+data <- read.csv("data/tidied/england_la_count.csv") %>%
   tbl_df
 
+ex <- data  %>% 
+  select(la=lad2013_code, year, sex, age, deaths, population)  %>% 
+  group_by(sex, la, year)  %>% 
+  summarise(
+    e50=sum(age[age>=50]*deaths[age>=50])/sum(deaths[age>=50]), 
+    e65=sum(age[age>=65]*deaths[age>=65])/sum(deaths[age>=65]), 
+    e80=sum(age[age>=80]*deaths[age>=80])/sum(deaths[age>=80])
+    )
 
-data_tidied <- dta  %>% 
-  gather(key=label, value=count, -lad2013_code, -lad2013_name, -country, -sex, -age)  %>% 
+write.csv(ex, file="data/tidied/england_ex.csv", row.names=FALSE)
+
+
+# cumulative survival from age 50, 65, and 80
+
+dta <- data %>%
+  select(la=lad2013_code, year, sex, age, deaths, population)  %>% 
+  mutate(cmr=deaths/population) %>%
+  filter(!is.na(cmr)) %>%
+  group_by(la, sex, year) %>%
+  arrange(la, sex, year, age) %>%
   mutate(
-    year = str_extract(label, "\\d{1,4}"), 
-    category = str_replace(str_extract(label, "\\D*"), "_$", ""),
-    sex=ifelse(sex==1, "male", "female"))   %>% 
-  select(lad2013_code, country, sex, age, year, category, count) %>%
-  spread(category, count)
+    L = 1
+    ) 
+    
 
-write.csv(data_tidied, file="data/tidied/england_la_count.csv", row.names=FALSE)
+  
+
