47ecfe853b418dcc3a5942e3fe45df2d43074ed9
Jonathan Minton <nate.minton@gmail.com>
2015-06-17 00:00:29 +0100

    start to creat per capita spend estimates

--- scripts/analyses_of_la_changes.R
+++ scripts/analyses_of_la_changes.R
@@ -28,7 +28,8 @@ dta <- read_csv(file = "data/care_cuts/combined_linked_and_tidied_r03.csv")
 
 # keep only the following inner categories
 
-dta <- dta %>% filter(inner %in% 
+dta <- dta %>% filter(outer =="Adults" & 
+                        inner %in% 
                  c(
                    "adults_65plus",
                    "adults_u65_learning",
@@ -49,6 +50,49 @@ ecode_lookup <- ecode_lookup  %>%
 dta <- dta %>% inner_join(ecode_lookup) %>% filter(class !="SD")
 
 tot_exp <- dta %>% filter(expense_type == "total_expenditure")
+tot_exp <- tot_exp %>% select(ons_code, year= start_year, inner, amount) %>% 
+  group_by(ons_code, year, inner) %>% summarise(amount=sum(amount)) %>% 
+  ungroup
+
+# Now to make per (appropriate) capita
+dta_pop_counts <- read_csv("data/tidied/england_la_count.csv")
+
+pop_by_broad_age_group <- dta_pop_counts %>% 
+  select(ons_code = lad2013_code, sex, age, year, population) %>%
+  group_by(ons_code, year) %>% 
+  summarise(
+    pop_0_17 = sum(population[age >= 0 & age < 17]),
+    pop_18_64 = sum(population[age >= 18 & age <= 64]),
+    pop_50_64 = sum(population[age >= 50 & age <= 64]),
+    pop_65_plus = sum(population[age >=65]),
+    pop_80_plus = sum(population[age >=80])
+  )
+
+tot_exp <- tot_exp %>% 
+  spread(key="inner", value = "amount")
+
+per_cap_spend <- tot_exp %>% inner_join(pop_by_broad_age_group) %>% 
+  mutate(
+    adults_65_pc = 1000 * adults_65plus / pop_65_plus,
+    adults_us6_learning_pc = 1000 * adults_u65_learning / pop_18_64,
+    adults_u65_mental_pc = 1000 * adults_u65_mental / pop_18_64,
+    adults_u65_other_pc = 1000 * adults_u65_other / pop_18_64,
+    adults_u65_physical_pc = 1000 * adults_u65_physical / pop_18_64,
+    adults_u65_social_care_pc = 1000 * adults_u65_social_care / pop_18_64
+) %>% select(ons_code, year, contains("pc"))
+
+per_cap_spend %>% 
+  gather(key="type", value="amount", -ons_code, -year) %>% 
+  group_by(year, type) %>% summarise(
+    amount_upper = mean(amount) + 2 * sd(amount),
+    amount_lower = mean(amount) - 2 * sd(amount),
+    amt_mean = mean(amount),
+    amt_median = median(amount),
+    amt_lq = quantile(amount, 0.025),
+    amt_uq = quantile(amount, 0.975)
+    ) %>% ggplot(.) +
+  geom_line(aes(x=year, y=amt_median)) + 
+  facet_wrap(~ type, scales="free")
 
 
 tot_exp %>% 
